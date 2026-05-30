0837fbd6811ebfec1cf4e8894cc091816fbc5f78
O-076 <omareslamg@gmail.com>
2026-05-30 01:02:49 +0000

    Third copilot commit

--- comments.js
+++ comments.js
@@ -1,60 +1,42 @@
-// create web server
-const express = require('express');
-const app = express();
-app.use(express.json());
-const port = 3000;
+// create web server with raw java script
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// create comments array
-let comments = [];
-
-// create comment
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.status(201).send(comment);
-});
-
-// get all comments
-app.get('/comments', (req, res) => {
-    res.send(comments);
-});
-
-// get comment by id
-app.get('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    const comment = comments.find(c => c.id === id);
-    if (!comment) {
-        return res.status(404).send('Comment not found');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/comments') {
+        if (req.method === 'POST') {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                const comment = JSON.parse(body);
+                // Here you would typically save the comment to a database
+                console.log('Received comment:', comment);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Comment received' }));
+            });
+        } else {
+            res.writeHead(405);
+            res.end('Method Not Allowed');
+        }
+    } else {
+        res.writeHead(404);
+        res.end('Not Found');
     }
-    res.send(comment);
 });
 
-// update comment by id
-app.put('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    const comment = comments.find(c => c.id === id);
-    if (!comment) {
-        return res.status(404).send('Comment not found');
-    }
-    Object.assign(comment, req.body);
-    res.send(comment);
-});
-
-// delete comment by id
-app.delete('/comments/:id', (req, res) => {
-    const id = req.params.id;
-    const index = comments.findIndex(c => c.id === id);
-    if (index === -1) {
-        return res.status(404).send('Comment not found');
-    }
-    comments.splice(index, 1);
-    res.status(204).send();
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the Comments API');
-});
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+})
